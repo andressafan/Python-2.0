81e574870f739b34ec5c7438aabe9a7adaf084ab
Andressa Felix Almeida Nascimento <andressafelix@outlook.com>
2023-07-05 02:12:02 -0300

    Update d065.py

--- Mundo002/Desafios002/d065.py
+++ Mundo002/Desafios002/d065.py
@@ -4,3 +4,28 @@ os valores e qual foi o maior e o menor valor lido. O programa
 deve perguntar ao usuário se ele quer ou não continuar a digitar valores'''
 
 
+n = int(input('Digite um número: '))
+p = 'S'
+soma = n
+c = 1
+maior = menor = n
+
+while p != 'N':
+    n = int(input('Digite outro número: '))
+    if n > maior:
+        maior = n
+    elif n < menor:
+        menor = n
+    p = str(input('''
+Deseja adicionar mais números?
+Se sim digite qualquer letra, se não digite "N": ''')).upper()
+    soma += n
+    c += 1
+
+media = soma/c
+print(f'''
+Você adicionou {c} números
+Média: {media}
+Maior: {maior}
+Menor: {menor}''')
+
