d34178ba92ee12f9c5752dbfa22c3e1cf59d75e3
Andressa Felix Almeida Nascimento <andressafelix@outlook.com>
2023-07-05 14:15:56 -0300

    Update Teoria15.py

---
+++ Mundo002/Teoria15.py
@@ -0,0 +1,19 @@
+# Interrompendo repetições while
+
+
+#                _🏆_
+# ____  _💰_  _____💰_  _💰_______...
+
+# estrutura infinita
+
+# enquanto Verdadeiro                     while True:
+#   se chão                                   if chao:
+#       passo                                    passo
+#   se buraco                                 if buraco:
+#       pula                                     pula
+#   se moeda                                  if moeda:
+#       pega                                     pega
+#   se troféu                                 if troféu:
+#       pula                                     pula
+#       imterrompa                               break
+# pega                                     pega
