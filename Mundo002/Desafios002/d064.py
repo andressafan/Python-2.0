7d5deb9af0817d23e1479f9370533fcfa0034f18
Andressa Felix Almeida Nascimento <andressafelix@outlook.com>
2023-07-05 01:49:50 -0300

    update
    update

--- Mundo002/Desafios002/d064.py
+++ Mundo002/Desafios002/d064.py
@@ -3,3 +3,14 @@ pelo teclado. O programa só vai parar quando o usuário
 digitar o valor 999, que é a condição de parada. No final,
 mostre quantos números foram digitados e qual foi a soma entre eles
 (desconsiderando a flag)'''
+
+print(f'{" Desafio 064 ":=^30}')
+n = int(input('Digite um número ou digite "999" para parar: '))
+c = 1
+soma = n
+while n != 999:
+    n = int(input('Digite outro número ou "999" para parar: '))
+    if n != 999:
+        c += 1
+        soma += n
+print(f'Você digitou {c} números e a soma entre eles é: {soma}')
