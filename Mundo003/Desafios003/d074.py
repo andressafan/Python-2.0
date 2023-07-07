5f01ad5c71ebb5bd3e1391f071d97ec0cb99d50c
Andressa Felix Almeida Nascimento <andressafelix@outlook.com>
2023-07-07 13:48:19 -0300

    Update d074.py

--- Mundo003/Desafios003/d074.py
+++ Mundo003/Desafios003/d074.py
@@ -7,10 +7,27 @@ indique o menor e o maior valor que estão na tupla.
 """
 
 print(f'{" Desafio 074 ":=^30}')
-import random
+from random import randint
 
-tupla = ()
-for i in range(6):
-    n = random.randint(1, 100)
-    tupla = input(n)
-print(tupla)
+n = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+print(f'Valores sorteados: {n}')
+menor = 999
+maior = -999
+for i in n:  # passa pelos elementos da tuplas
+    # for i in (len(n)) # passa pelos índices dos elementos da tupla
+    if i > maior:
+        maior = i
+        if maior > maior:
+            maior = maior
+    elif i < menor:
+        menor = i
+        if menor > menor:
+            menor = menor
+
+print(f'''
+O menor número: {menor}
+O maior número: {maior}''')
+
+print(f'''
+O menor número: {min(n)}
+O maior número: {max(n)}''')
