8309cf7c4a834a3ce532f90327b6e5b5516e09f2
Andressa Felix Almeida Nascimento <andressafelix@outlook.com>
2023-07-07 14:51:15 -0300

    update

--- Mundo003/Desafios003/d076.py
+++ Mundo003/Desafios003/d076.py
@@ -10,4 +10,13 @@ Lápis ---------- R$ 1.75
 Caneta --------- RS 0.50
 """
 
-print(f'{" Desafio 076 ":=^30}')
+print(f'{" Desafio 076 ":=^35}')
+tupla = ('Lápis', 1.75, 'Caneta', 2.00, 'Estojo', 25.00, 'Caderno'
+         , 30.00, 'Borracha', 0.50, 'Grafite', 0.75, 'Resma Folha A4', 20.00
+         , 'Régua', 5.00)
+print(f'{" Tabela de Preços ":=^35}')
+
+for i in range(0, len(tupla), 2):
+    nome = tupla[i]
+    preco = tupla[i+1]
+    print(f"{f'{nome:-<15}' + '-' * 10 + f'R$ {preco:<4.2f}':^35}")
--- Mundo003/Desafios003/d077.py
+++ Mundo003/Desafios003/d077.py
@@ -5,3 +5,20 @@ cada palavra, quais são suas vogais.
 """
 
 print(f'{" Desafio 077 ":=^30}')
+
+tupla = ('agua', 'arroz', 'feijao', 'cafe', 'pao', 'maça', 'carne'
+         , 'bombom', 'suco')
+
+for i in range(len(tupla)):
+    print(f'A palavra {tupla[i]} tem as vogais:', end='')
+    if 'a' in tupla[i]:
+        print(f'a', end='')
+    if 'e' in tupla[i]:
+        print(f'e', end='')
+    if 'i' in tupla[i]:
+        print(f'i', end='')
+    if 'o' in tupla[i]:
+        print(f'o', end='')
+    if 'u' in tupla[i]:
+        print(f'u', end='')
+    print('\n')
