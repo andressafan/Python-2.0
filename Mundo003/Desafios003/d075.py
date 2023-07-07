142dcc53db0f77f9bb5236ebd5bd9cb129ca38c8
Andressa Felix Almeida Nascimento <andressafelix@outlook.com>
2023-07-07 14:08:29 -0300

    Update d075.py

--- Mundo003/Desafios003/d075.py
+++ Mundo003/Desafios003/d075.py
@@ -8,3 +8,16 @@ C) Quais foram os números pares.
 """
 
 print(f'{" Desafio 075 ":=^30}')
+
+n = (int(input('Digite um número: ')), int(input('Digite outro número: ')), int(input('Digite outro número: ')),
+     int(input('Digite outro número: ')))
+print(f'Você digitou os valores {n}')
+print(f'O número 9 apareceu {n.count(9)} vezes')
+if 3 in n:
+    print(f'O número 3 apareceu na posição {n.index(3) + 1}')
+else:
+    print(f'O número 3 não aparece em nenhuma posição!')
+print(f'Os números pares são: ', end='')
+for i in n:
+    if i % 2 == 0:
+        print(f'{i}, ', end='')
