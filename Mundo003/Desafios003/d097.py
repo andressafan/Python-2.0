01e077e69afaa25266b94aa8159788cbac8eda2a
Andressa Felix Almeida Nascimento <andressafelix@outlook.com>
2023-07-28 23:04:08 -0300

    Update d097.py

--- Mundo003/Desafios003/d097.py
+++ Mundo003/Desafios003/d097.py
@@ -4,9 +4,9 @@ que receba um texto qualquer como parâmetro e mostre uma mensagem
 com tamanho adaptável.
 Ex: escreva('Olá, Mundo!')
 Saída:
---------------
-Olá, Mundo!
---------------
+---------------
+  Olá, Mundo!
+---------------
 """
 
 print(f'{" Desafio 097 ":=^50}')
