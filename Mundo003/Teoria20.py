fe76f23229ed42c14a725660bda914ebe6386397
Andressa Felix Almeida Nascimento <andressafelix@outlook.com>
2023-07-28 22:11:34 -0300

    update

---
+++ Mundo003/Teoria20.py
@@ -0,0 +1,47 @@
+# Funções
+
+# Rotina
+# def -> usado para declarar uma função personalizada
+
+# print('-----------------------------') ---> se repete
+# print('Sistema de Alunos')
+# print('-----------------------------')
+
+# print('-----------------------------')
+# print('Cadastro de Funcionários')
+# print('-----------------------------')
+
+# print('-----------------------------')
+# print('Erro do Sistema')
+# print('-----------------------------')
+
+
+# def mostraLinha(): --> função é definida
+#   print('-----------------------------')
+
+
+# mostraLinha() --> função é chamada
+# print('Sistema de Alunos')
+# mostraLinha()
+# print('Cadastro de Funcionários')
+# mostraLinha()
+# print('Erro do Sistema')
+# mostraLinha()
+
+# a função deve ser chamada para ser executada!
+
+
+# Ultilizando Parâmetros:
+
+# def mensagem(msg): ---> msg = parâmetro
+#   print('-----------------------------')
+#   print(msg)
+#   print('-----------------------------')
+
+# mensagem('Sistema de Alunos') --> chama a função e passa o parâmetro
+
+ 
+
+
+
+
