7eab33c4bd32488aea7a8d5aef811277d4e6d3e7
Andressa Felix Almeida Nascimento <andressafelix@outlook.com>
2023-10-11 19:04:36 -0300

    ok

---
+++ Mundo003/Teoria21.py
@@ -0,0 +1,18 @@
+# Funções
+# Parte 2
+
+# > Interactive Help
+# > Docstrings
+# > Argumentos Opcionais
+# > Escopo de Variáveis
+# > Retorno de Resultados
+
+# Interactive Help
+# Ajuda no Python:
+# Console Pyhton
+# help()
+# escolha a função para ver a documentação
+
+# ou
+# help(print)
+
